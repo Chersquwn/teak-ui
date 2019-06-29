66a6a2ca179f7d601b36e70d222b3bf1d75233af
Chersquwn <Chersquwn@163.com>
2019-06-29 20:48:56 +0800

    feat: add Mask component

---
+++ packages/Mask/index.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponentElement } from 'react'
+import { Fade } from 'teak-ui/Animation'
+
+import './index.scss'
+
+export interface MaskProps {
+  zIndex?: number
+  visible?: boolean
+  onClick?: () => void
+  duration: number
+}
+
+const Mask = (props: MaskProps): FunctionComponentElement<MaskProps> => {
+  const { visible = true, onClick, zIndex = 10000, duration = 300 } = props
+  const classPrefix = 'teak-mask'
+
+  return (
+    <Fade appear in={visible} timeout={duration}>
+      <div
+        className={`${classPrefix}`}
+        style={{ zIndex }}
+        onClick={onClick ? onClick : null}
+      />
+    </Fade>
+  )
+}
+
+export default Mask
