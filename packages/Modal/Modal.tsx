0d712e036f146dff914f5bdbca2dd1bc64989e3a
Chersquwn <Chersquwn@163.com>
2019-07-03 22:20:04 +0800

    feat: add Modal component

---
+++ packages/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode, FunctionComponent } from 'react'
+import Popup from 'teak-ui/Popup'
+
+export interface ModalProps {
+  visible: boolean
+  header?: ReactNode
+  footer?: ReactNode
+  children: ReactNode
+  onClosed?: () => void
+}
+
+const Modal: FunctionComponent<ModalProps> = (props: ModalProps) => {
+  const { header, footer, children, ...restProps } = props
+  const classPrefix = 'teak-modal'
+
+  return (
+    <Popup className={`${classPrefix}`} {...restProps}>
+      <div className={`${classPrefix}-container`}>
+        {header && <div className={`${classPrefix}-header`}>{header}</div>}
+        {<div className={`${classPrefix}-content`}>{children}</div>}
+        {footer && <div className={`${classPrefix}-footer`}>{footer}</div>}
+      </div>
+    </Popup>
+  )
+}
+
+export default Modal
